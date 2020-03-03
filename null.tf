c7c392953a575464d8d307a03c0b4d5f9f6b69f5
Hugo GARBEZ <hugo.garbez.partner@decathlon.com>
2020-03-03 13:57:03 +0100

    Update null.tf

--- null.tf
+++ null.tf
@@ -1,4 +1,20 @@
-resource "null_resource" "ConfigureAnsibleLabelVariable" {
+resource "null_resource" "test_ressource" {
+  provisioner "local-exec" {
+    command = "echo 'Ligne 1'"
+  }
+  provisioner "local-exec" {
+    command = "echo 'Ligne 2'"
+  }
+  provisioner "local-exec" {
+    command = "echo 'Sleep 15'; sleep 15; echo 'fin sleep 15'"
+  }
+  provisioner "local-exec" {
+    command = "echo 'Finish'"
+  }
+}
+
+
+resource "null_resource" "test_var" {
   provisioner "local-exec" {
     command = "echo 'Ligne 1'"
   }
