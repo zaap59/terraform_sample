06b0b45d27da1a0a89fc9fd4632f0b25d0ea655f
Hugo GARBEZ <zaap59@gmail.com>
2021-05-13 18:21:32 +0200

    Update null.tf

--- null.tf
+++ null.tf
@@ -1,11 +1,19 @@
-resource "null_resource" "test_ressource_preprod" {
-  provisioner "local-exec" {
-    command = "echo 'PREPRODUCTION'"
+data "http" "example" {
+  url = "https://api.coincap.io/v2/assets/bitcoin"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
   }
-  provisioner "local-exec" {
-    command = "echo 'Ligne 2'"
-  }
-  provisioner "local-exec" {
-    command = "echo 'Finish PREPRODUCTION'"
-  }
+}
+
+locals {
+  data = jsondecode(data.http.example.body).data
+  price = local.data.priceUsd
+  name = local.data.name
+  symbol = local.data.symbol
+}
+
+output "Crypto" {
+  value = "${local.name}, symbol: ${local.symbol}, priceUsd: ${local.price}"
 }
